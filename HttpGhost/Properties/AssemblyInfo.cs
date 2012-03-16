0b5478928d1ebdeb58a864cc923652c7b4f1331d
Daniel Vinntreus <gudski@gmail.com>
2012-03-16 21:03:19 +0100

    Update assemblyinfo

--- HttpGhost/Properties/AssemblyInfo.cs
+++ HttpGhost/Properties/AssemblyInfo.cs
@@ -5,14 +5,15 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RestInspector")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("HttpGhost")]
+[assembly: AssemblyDescription("Simplify integrationtesting via http.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("RestInspector")]
-[assembly: AssemblyCopyright("Copyright © Daniel Persson 2011")]
+[assembly: AssemblyProduct("HttpGhost")]
+[assembly: AssemblyCompany("Daniel Vinntreus")]
+[assembly: AssemblyCopyright("Copyright © Daniel Vinntreus 2011-2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: InternalsVisibleTo("RestInspector.IntegrationTests")]
+[assembly: InternalsVisibleTo("IntegrationTests")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
