b7721892c7c384cf7c421af902cb9eeab88d32bf
Daniel Vinntreus <gudski@gmail.com>
2012-07-27 16:39:21 +0200

    Handle simple restricted ones. close #22

--- HttpGhost/Navigation/WebRequestNavigator.cs
+++ HttpGhost/Navigation/WebRequestNavigator.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Diagnostics;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -17,6 +16,7 @@ namespace HttpGhost.Navigation
         {
             watch = new Stopwatch();
         }
+
         public INavigationResult Navigate(IRequest request)
         {
             if (string.IsNullOrEmpty(request.Method))
@@ -25,85 +25,38 @@ namespace HttpGhost.Navigation
             }
             watch.Start();
 
-            var httpWebRequest = BuildHttpWebRequest(request);
-            
+            var httpWebRequest = HttpWebRequestBuilder.BuildFrom(request);
             var httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
 
             watch.Stop();
 
             var response = BuildResponse(httpWebResponse);
-            if(IsRedirect(response.StatusCode))
+
+            if (IsRedirect(response.StatusCode))
             {
-                var redirect = BuildRedirectRequest(request, response);
-                return Navigate(redirect);
+                var url = GetUrlForRedirect(request.Url, response);
+                return Navigate(new Request(url, "GET", request.Headers));
             }
+
             watch.Reset();
+
             return new NavigationResult(request, response)
             {
                 TimeSpent = watch.ElapsedMilliseconds
             };
         }
 
-        private static readonly string[] RestrictedHeaders = new[]
+        private static string GetUrlForRedirect(string baseUrl, IResponse response)
         {
-            "Content-Type", 
-            "Accept",
-            "Connection",
-            "Content-Length",
-            "Date",
-            "Expect",
-            "Host"
-        };
-
-        public static HttpWebRequest BuildHttpWebRequest(IRequest request)
-        {
-            var httpWebRequest = (HttpWebRequest) WebRequest.Create(request.Url);
-            foreach (var key in request.Headers.AllKeys.Except(RestrictedHeaders))
-            {
-                httpWebRequest.Headers.Add(key, request.Headers[key]);
-            }
-            httpWebRequest.Method = request.Method;
-            httpWebRequest.AllowAutoRedirect = false;
-            httpWebRequest.ContentType = request.Headers[HttpRequestHeader.ContentType];
-            httpWebRequest.Accept = request.Headers[HttpRequestHeader.Accept];
-            httpWebRequest.Connection = request.Headers[HttpRequestHeader.Connection];
-            httpWebRequest.Expect = request.Headers[HttpRequestHeader.Expect];
-            
-            if(request.Headers[HttpRequestHeader.Date] != null)
-            {
-                httpWebRequest.Date = DateTime.Parse(request.Headers[HttpRequestHeader.Date]);
-            }
-
-            if (!string.IsNullOrEmpty(request.Body))
-            {
-                var bytes = Encoding.ASCII.GetBytes(request.Body);
-                httpWebRequest.ContentLength = bytes.Length;
-                using (var requestStream = httpWebRequest.GetRequestStream())
-                {
-                    requestStream.Write(bytes, 0, bytes.Length);
-                }
-            }
-
-            return httpWebRequest;
+            return new Uri(new Uri(baseUrl), response.Headers["location"]).ToString();
         }
 
-        private static Request BuildRedirectRequest(IRequest request, IResponse response)
+        private static bool IsRedirect(HttpStatusCode httpStatusCode)
         {
-            var uri = new Uri(new Uri(request.Url), response.Headers["location"]);
-            var redirect = new Request(uri.ToString());
-            foreach (var key in request.Headers.AllKeys)
-            {
-                redirect.AddHeader(key, request.Headers.Get(key));
-            }
-            redirect.Method = "GET";
-            return redirect;
+            var statusCode = (int) httpStatusCode;
+            return statusCode.IsBetween(299, 400);
         }
-
-        private static bool IsRedirect(HttpStatusCode statusCode)
-        {
-            return ((int)statusCode).ToString(CultureInfo.InvariantCulture).StartsWith("3");
-        }
-
+        
         private static IResponse BuildResponse(HttpWebResponse httpWebResponse)
         {
             var responseStream = httpWebResponse.GetResponseStream();
@@ -111,7 +64,7 @@ namespace HttpGhost.Navigation
                 return null;
 
             var response = new Response();
-            using(var s = new StreamReader(responseStream))
+            using (var s = new StreamReader(responseStream))
             {
                 response.Body = s.ReadToEnd();
             }
@@ -122,5 +75,98 @@ namespace HttpGhost.Navigation
             response.StatusCode = httpWebResponse.StatusCode;
             return response;
         }
+    }
+
+    public static class IntegerExtensions
+    {
+        public static bool IsBetween(this int i, int greaterThan, int lessThan)
+        {
+            return i > greaterThan && i < lessThan;
+        }
+    }
+
+
+    public static class HttpWebRequestBuilder
+    {
+        public static HttpWebRequest BuildFrom(IRequest request)
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(request.Url);
+
+            httpWebRequest.AddAllowedHeaders(request);
+            httpWebRequest.Method = request.Method;
+            httpWebRequest.AllowAutoRedirect = false;
+            httpWebRequest.ContentType = request.Headers[HttpRequestHeader.ContentType];
+            httpWebRequest.Accept = request.Headers[HttpRequestHeader.Accept];
+            httpWebRequest.Connection = request.Headers[HttpRequestHeader.Connection];
+            httpWebRequest.Expect = request.Headers[HttpRequestHeader.Expect];
+            httpWebRequest.Referer = request.Headers[HttpRequestHeader.Referer];
+            httpWebRequest.SendChunked = request.Headers[HttpRequestHeader.TransferEncoding] != null;
+            httpWebRequest.TransferEncoding = request.Headers[HttpRequestHeader.TransferEncoding];
+            httpWebRequest.UserAgent = request.Headers[HttpRequestHeader.UserAgent];
+            httpWebRequest.AddDateHeader(request);
+            httpWebRequest.AddIfModifiedSinceHeader(request);
+            httpWebRequest.AddBody(request.Body);
+
+            return httpWebRequest;
+        }
+    }
+
+    internal static class HttpWebRequestExtensions
+    {
+        public static void AddAllowedHeaders(this HttpWebRequest httpWebRequest, IRequest request)
+        {
+            foreach (var key in request.Headers.AllKeys.Except(RestrictedHeaders))
+            {
+                httpWebRequest.Headers.Add(key, request.Headers[key]);
+            }
+        }
+
+        public static void AddDateHeader(this HttpWebRequest httpWebRequest, IRequest request)
+        {
+            if (request.Headers[HttpRequestHeader.Date] != null)
+            {
+                httpWebRequest.Date = DateTime.Parse(request.Headers[HttpRequestHeader.Date]);
+            }
+        }
+
+        public static void AddIfModifiedSinceHeader(this HttpWebRequest httpWebRequest, IRequest request)
+        {
+            if (request.Headers[HttpRequestHeader.IfModifiedSince] != null)
+            {
+                httpWebRequest.IfModifiedSince = DateTime.Parse(request.Headers[HttpRequestHeader.IfModifiedSince]);
+            }
+        }
+
+        public static void AddBody(this HttpWebRequest httpWebRequest, string body)
+        {
+            if (!string.IsNullOrEmpty(body))
+            {
+                var bytes = Encoding.ASCII.GetBytes(body);
+                httpWebRequest.ContentLength = bytes.Length;
+                using (var requestStream = httpWebRequest.GetRequestStream())
+                {
+                    requestStream.Write(bytes, 0, bytes.Length);
+                }
+            }
+        }
+
+        private static readonly string[] RestrictedHeaders = new[]
+        {
+            "Content-Type", 
+            "Accept",
+            "Connection",
+            "Date",
+            "Expect",
+            "If-Modified-Since",
+            "Referer",
+            "Transfer-Encoding",
+            "User-Agent",
+            
+            //not implemented handling
+            "Range",
+            //these do not seem to actually set headers on webrequest (at least from unit-tests)
+            "Content-Length",
+            "Host"
+        };
     }
 }
--- UnitTests/Navigation/HttpWebRequestBuilderTests.cs
+++ UnitTests/Navigation/HttpWebRequestBuilderTests.cs
@@ -6,7 +6,7 @@ using NUnit.Framework;
 namespace UnitTests.Navigation
 {
     [TestFixture]
-    public class WebRequestNavigatorTests
+    public class HttpWebRequestBuilderTests
     {
         private Request request;
         private const string SOME_URL = "http://a";
@@ -22,12 +22,15 @@ namespace UnitTests.Navigation
         [TestCase(HttpRequestHeader.Connection, "a")]
         [TestCase(HttpRequestHeader.Date, "2000-01-01")]
         [TestCase(HttpRequestHeader.Expect, "a")]
-        
+        [TestCase(HttpRequestHeader.IfModifiedSince, "2000-01-01")]
+        [TestCase(HttpRequestHeader.Referer, "a")]
+        [TestCase(HttpRequestHeader.TransferEncoding, "a")]
+        [TestCase(HttpRequestHeader.UserAgent, "a")]
         public void BuildWebRequest_RestrictedTypes_AddsCorrectHeader(HttpRequestHeader header, string value)
         {
             request.AddHeader(header, value);
 
-            var webrequest = WebRequestNavigator.BuildHttpWebRequest(request);
+            var webrequest = HttpWebRequestBuilder.BuildFrom(request);
 
             Assert.That(webrequest.Headers[header], Is.Not.Null);
         }
