9046fdcb151b2fba6266ed24f1fc2e01a0b244dd
Daniel Vinntreus <gudski@gmail.com>
2012-09-09 12:43:12 +0200

    update ctor as httpsession

--- HttpGhost/HtmlSession.cs
+++ HttpGhost/HtmlSession.cs
@@ -1,11 +1,31 @@
 using System;
+using HttpGhost.Authentication;
 using HttpGhost.Navigation;
+using HttpGhost.Serialization;
 using HttpGhost.Transport;
 
 namespace HttpGhost
 {
     public class HtmlSession : Session<IHtmlResult>
     {
+         /// <summary>
+		/// Uses Anonymous authentication
+		/// </summary>
+		public HtmlSession(){}
+
+		/// <summary>
+		/// Uses Basic Authentication.
+		/// </summary>
+		public HtmlSession(string username, string password) : base(username, password){}
+        
+        /// <summary>
+        /// Uses provided authentication mechanism and navigator which uses .Net webclient for requests and a default serializer
+        /// </summary>
+        /// <param name="authentication"></param>
+        public HtmlSession(IAuthenticate authentication) : base(authentication){}
+
+        public HtmlSession(IAuthenticate authentication, INavigate navigator, ISerializeBuilder serializeBuilder) : base(authentication, navigator, serializeBuilder) { }
+
         protected override IHtmlResult Navigate(IRequest request)
         {
             return new HtmlResult(navigator.Navigate(request))
--- HttpGhost/JsonSession.cs
+++ HttpGhost/JsonSession.cs
@@ -1,9 +1,30 @@
+using HttpGhost.Authentication;
+using HttpGhost.Navigation;
+using HttpGhost.Serialization;
 using HttpGhost.Transport;
 
 namespace HttpGhost
 {
     public class JsonSession : Session<IJsonResult>
     {
+         /// <summary>
+		/// Uses Anonymous authentication
+		/// </summary>
+		public JsonSession(){}
+
+		/// <summary>
+		/// Uses Basic Authentication.
+		/// </summary>
+		public JsonSession(string username, string password) : base(username, password){}
+        
+        /// <summary>
+        /// Uses provided authentication mechanism and navigator which uses .Net webclient for requests and a default serializer
+        /// </summary>
+        /// <param name="authentication"></param>
+        public JsonSession(IAuthenticate authentication) : base(authentication){}
+
+        public JsonSession(IAuthenticate authentication, INavigate navigator, ISerializeBuilder serializeBuilder) : base(authentication, navigator, serializeBuilder) { }
+
         /// <summary>
         /// Get with content-type = application/json
         /// </summary>
